95e75a615a863c1b3f4ab86e81078684af445139
MR.GRIMX72 <69077592+candranovanmahendra@users.noreply.github.com>
2025-06-29 19:18:52 +0700

    Update facebook.js

--- public/facebook.js
+++ public/facebook.js
@@ -1,14 +1,20 @@
-const getFbVideoInfo = require("fb-downloader-scrapper")
+const getFbVideoInfo = async (url) => {
+  const { default: downloader } = await import('fb-downloader-scrapper');
+  return downloader(url);
+};
+
 require("../settings.js")
 
 async function fbdl(fbUrl) {
-try {
+  try {
     const res = await getFbVideoInfo(fbUrl);
-    return res
-    } catch (error) {
-    return console.log(error)
-    }
+    return res;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
 }
 
-module.exports = { fbdl }
+module.exports = { fbdl };
+
 
